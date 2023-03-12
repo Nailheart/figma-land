4b892ed6d26b73cf2170881e79931b84c2ed12a7
Yaroslav Lebedenko <nailheart4@gmail.com>
2023-03-12 17:21:21 +0200

    + html, css

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
 (() => {
+  // Stiky header
+  const header = document.querySelector('.header');
+  if (header) {
+    window.addEventListener('scroll', () => {
+      if (window.pageYOffset > header.offsetTop) {
+        header.classList.add('header--sticky');
+      } else {
+        header.classList.remove('header--sticky');
+      }
+    });
+  }
+
   // Toggle navigation
   const nav = document.querySelector('.nav');
   if (nav) {
@@ -10,4 +22,43 @@
       body.classList.toggle('overflow-hidden');
     });
   }
+  
+  // Partners slider
+  const partnersSwiper = new Swiper('.partners__swiper', {
+    slidesPerView: 1,
+    loop: true,
+    speed: 3000,
+    spaceBetween: 24,
+
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  });
+
+  // Swiper
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 })();
